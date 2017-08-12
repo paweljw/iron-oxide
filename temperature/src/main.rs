8758f46beda546182901b24f5477609b008348c1
Paweł J. Wal <pawel.wal@codesthq.com>
2017-08-12 21:52:13 +0200

    Fancify temperature converter

--- temperature/src/main.rs
+++ temperature/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process;
 
 fn fahrenheit_to_celsius(fahrenheit: i32) -> i32 {
     (fahrenheit - 32) * 5 / 9
@@ -8,15 +9,7 @@ fn celsius_to_fahrenheit(celsius: i32) -> i32 {
     celsius * 9 / 5 + 32
 }
 
-fn main() {
-    println!("Please provide mode of (F, C)");
-    let mut mode = String::new();
-
-    io::stdin().read_line(&mut mode)
-        .expect("Failed to read line");
-
-    let mode = mode.trim();
-
+fn get_temperature() -> i32 {
     println!("Please provide temperature");
 
     let mut temperature = String::new();
@@ -24,13 +17,29 @@ fn main() {
     io::stdin().read_line(&mut temperature)
         .expect("Failed to read line");
 
-    let temperature: i32 = match temperature.trim().parse() {
+    match temperature.trim().parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Invalid temperature");
-            return;
+            println!("Invalid temperature, bye");
+            process::exit(1);
         }
-    };
+    }
+}
+
+fn get_mode() -> String {
+    println!("Is the temperature in F or C?");
+    let mut mode = String::new();
+
+    io::stdin().read_line(&mut mode)
+        .expect("Failed to read line");
+
+     String::from(mode.trim())
+}
+
+fn main() {
+
+    let temperature = get_temperature();
+    let mode = get_mode();
 
     if mode == "F" {
         println!("{}F is {}C", temperature, fahrenheit_to_celsius(temperature))
